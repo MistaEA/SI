7da06f143265a22695548543067e7731ce1ff5ce
MistaEA <90008089+MistaEA@users.noreply.github.com>
2022-02-11 00:05:36 -0600

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,20 +1,26 @@
-create database BIBILIOTECA;
-use BIBILIOTECA;
+create database BIBLIOTECA;
+use BIBLIOTECA;
 
 create table Libros (
     idLibro int primary key not null,
-    autor varchar (20),
+    titulo varchar (45),
+	autor varchar (20),
     editorial varchar (15),
     edicion varchar (20),
     anio int, 
     vols int,
-    genero varchar (15));
+    genero varchar (15),
+	descripcion varchar (100),
+	Catalogacion varchar(20),
+	Seccion varchar (10));
 
 create table Alumnos (
     matricula int primary key not null,
     nombreAlu varchar (10),
     apellidoAlu varchar (10),
-    telefono varchar (10));
+    telefono varchar (10),
+	especialidad varchar (20),
+	Observacion varchar(60));
 
 create table Prestamos(
     idPrestamo int primary key not null,
@@ -27,18 +33,22 @@ create table Prestamos(
 	/*PROCEDIMIENTOS PARA AGREGAR INFORMACION*/
 CREATE PROCEDURE insertLibros
     @idlibro int,
+	@titulo varchar (45),
     @autor varchar (20),
     @editorial varchar (15),
     @edicion varchar (20),
     @anio int, 
     @vols int,
-    @genero varchar (15)
+    @genero varchar (15),
+	@descripcion varchar (100),
+	@catalogacion varchar (20),
+	@seccion varchar (10)
 
     as
     begin 
-    Insert Into Libros (idLibro,autor,editorial,edicion,anio,vols,genero)
-                values (@idlibro, @autor,@editorial,@edicion,@anio,@vols,@genero);
+    Insert Into Libros (idLibro,titulo,autor,editorial,edicion,anio,vols,genero,descripcion,Catalogacion,Seccion)
+                values (@idlibro, @titulo,@autor,@editorial,@edicion,@anio,@vols,@genero,@descripcion,@catalogacion,@seccion);
     Select * from Libros;
     end
-
+execute insertLibros 1,'Matar a un Ruiseñor', 'Harper Lee','Caurin','1ra Edicion','1960','0','Novela','','','';
     
